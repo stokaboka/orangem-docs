b5e8960f6574559909af17f6de387d1b0527487e
igorkhorev <igorhorev@gmail.com>
2018-07-26 12:00:27 +0300

    footer * added links * added styles

--- src/components/DocFooter.vue
+++ src/components/DocFooter.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="footer">
-
+        <article>
+            <header>
+                <span class="footer-section__header">Contacts:</span>
+            </header>
+            <div class="footer-contacts__container footer-contacts-item">
+                <span>E-Mail: <a href="mailto:igorhorev@gmail.com" target="_blank">igorhorev@gmail.com</a></span>
+                <span>Twitter: <a href="https://twitter.com/IgorHorev" target="_blank">@IgorHorev</a></span>
+                <span>Skype: igor.horev</span>
+            </div>
+        </article>
     </div>
 </template>
 
@@ -11,6 +20,15 @@ export default {
 </script>
 
 <style scoped>
+
+    a, a:link, a:visited, a:active {
+        text-decoration: none;
+        color: white;
+        border-bottom: 2px solid #888888;
+        display: inline-block;
+        line-height: 0.85;
+    }
+
 .footer {
 
     position: relative;
@@ -33,5 +51,21 @@ export default {
     font-weight: bold;
 
     padding: 1vmax 0px;
+}
+
+.footer-contacts__container {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: left;
+    align-items: left;
+
+    margin: 1vmax;
+
+    font-weight: normal;
+}
+
+.footer-contacts-item {
+    text-align: left;
 }
 </style>
